ceaa094b8d2c19b0c3ba555099bc133491b92648
mosh <myintmyatag23@outlook.com>
2026-02-08 02:29:16 -0600

    Deploy to specific page and enable github workflows

--- quasar.config.js
+++ quasar.config.js
@@ -44,7 +44,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/moshfolio/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
